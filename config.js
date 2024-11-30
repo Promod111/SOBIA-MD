ca7dae4ff1c58aa97a51a52cf40bce158cac129e
Promod111 <promodbavanka123@gmail.com>
2024-11-30 21:42:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,19 +5,19 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFT4d94Z92FkFJD6XveFCah9ekSXg6eT1TJQ&s",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/m1rf91.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY SIR/MAM SOBIA MD IS AVAILABLE FOR YOUR HELP",
+ALIVE_MSG: process.env.ALIVE_MSG || "9476033464 *`|| mage link eka st dala dennako bhh`*",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 OWNER_REACT: process.env.OWNER_REACT || "true",
-BOT_NAME: process.env.BOT_NAME || "SOBIA MD",
+BOT_NAME: process.env.BOT_NAME || "SITHU MD",
 };
